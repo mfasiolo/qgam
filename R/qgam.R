9972196143c0ce4d91eed819ebf653c994f54aa1
Matteo <matteo.fasiolo@gmail.com>
2016-04-19 17:06:42 +0100

    qgam() more memory-parsimonious now

--- R/qgam.R
+++ R/qgam.R
@@ -1,27 +1,75 @@
+#######
+### Fitting quantile gam model
+#######
+#' Fitting quantile gam model
+#' 
+#' @param \code{XXX} .
+#' @return XXX.
+#'
+#' @details XXX
+#'         
+#' @author Matteo Fasiolo <matteo.fasiolo@@gmail.com>.   
+#' @references Fasiolo and Wood XXX.           
+#' @export
+#' 
 
-qgam <- function(form, data, tau, err = 0.01, ncores = 1, control = list(), controlGam = list())
+qgam <- function(form, data, tau, lsigma = NULL, err = 0.01, ncores = 1, control = list(), controlGam = list())
 {
-  # Initial Gaussianfit
+  nt <- length(tau)
+  
+  # Initial Gaussian fit
   gausFit <- gam(form, data = data, control = controlGam)
   control[["gausFit"]] <- gausFit
   
-  # Selecting the learning rate sigma
-  learn <- tuneLearnFast(form = form, data = data, err = err, tau = tau, ncores = ncores, control = control)
+  # Output list
+  out <- list()
+  
+  if( is.null(lsigma) ) { # Selecting the learning rate sigma OR ....
+    learn <- tuneLearnFast(form = form, data = data, err = err, tau = tau, ncores = ncores, control = control)
+    lsigma <- learn$lsigma
+    out[["calibr"]] <- learn
+  } else { # ... use the one provided by the user
+    if( length(lsigma) == 1 ) {
+      lsigma <- rep(lsigma, nt)
+    } else {
+      if( length(lsigma) != nt ) stop("lsigma should either be scalar of a vector of length(tau) ")
+    } }
+  
+
+  # Selection lambda
+  lam <- err * sqrt(2*pi*gausFit$sig2) / (2*log(2)*exp(lsigma))
   
   # Fitting a quantile model for each tau
-  nt <- length(tau)
-  qfit <- lapply(1:nt, function(ii){
+  out[["fit"]] <- lapply(1:nt, function(ii){
     
-    lam <- err * sqrt(2*pi*gausFit$sig2) / (2*log(2)*exp(learn$lsigma[ii])) 
+    .out <- gam(form, family = logF(tau = tau[ii], lam = lam[ii], theta = lsigma[ii]), data = data)
     
-    out <-  gam(form, family = logF(tau = tau[ii], lam = lam, theta = learn$lsigma[ii]), data = data)
-    
-    return(out)
-    
+    # Removing data and smooth matrix to reduce memory requirements. There quantities
+    # are kept only inside the first fit ( qfit[[1]] )
+    if(ii > 1){
+      .out$model  <- NULL
+      .out$smooth <- NULL 
+    } 
+        
+    return( .out )
   })
   
-  return( qfit )
+  # Storing output list
+  names(out[["fit"]]) <- tau
+  out[["model"]] <- out[["fit"]][[1]][["model"]]
+  out[["smooth"]] <- out[["fit"]][[1]][["smooth"]]
+  out[["fit"]][[1]][["model"]] <- NULL
+  out[["fit"]][[1]][["smooth"]] <- NULL
+  
+  out[["tau"]] <- tau
+  out[["lambda"]] <- lam
+  out[["lsigma"]] <- lsigma
+  
+  return( out )
 }
+
+# fit$model
+# fit$smooth[[1]]$UZ   # fit$smooth
 
 # 
 # qFit <- lapply(1:nt, function(ii){
