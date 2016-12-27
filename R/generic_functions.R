ef0509b63233a9f781f88f7cf1179e9aeec296b4
Matteo Fasiolo <matteo.fasiolo@gmail.com>
2016-12-27 10:00:51 +0100

    Added generic check function

--- R/generic_functions.R
+++ R/generic_functions.R
@@ -1,4 +1,24 @@
-
+##########################
+#' Generic checking function
+#' 
+#' @description Generic function for checking R objects which produces, for instance, convergence tests or diagnostic plots. 
+#'              For \code{qgam} objects \code{check.qgam()} will be used. 
+#' @param obj the object to be checked. 
+#' @param ... extra arguments to be passed to the specific checking method used. 
+#' @return Reports the results of convergence tests and/or produces diagnostic plots.
+#' @author Matteo Fasiolo <matteo.fasiolo@@gmail.com>. 
+#' @examples
+#' #######
+#' # Using check.qgam
+#' #######
+#' library(qgam)
+#' set.seed(0)
+#' dat <- gamSim(1, n=200)
+#' b<-qgam(y~s(x0)+s(x1)+s(x2)+s(x3), data=dat, qu = 0.5)
+#' plot(b, pages=1)
+#' check(b, pch=19, cex=.3) 
+#' @export check
+#'
 check <- function (obj, ...) {
   UseMethod("check", obj)
 }
