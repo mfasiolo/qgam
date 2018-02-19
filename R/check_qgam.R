bfafa81a91925701d59401a9765614d06cab85b3
Matteo Fasiolo <matteo.fasiolo@gmail.com>
2018-02-19 14:32:58 +0000

    check.qgam now estimates smoothing bias

--- R/check_qgam.R
+++ R/check_qgam.R
@@ -2,20 +2,26 @@
 #' Some diagnostics for a fitted qgam model
 #' 
 #' @description Takes a fitted gam object produced by \code{qgam()} and produces some diagnostic information 
-#'              about the fitting procedure and results. It is a simplified version of \code{mgcv::gam.check}.
+#'              about the fitting procedure and results. It is partially based on \code{mgcv::gam.check}.
 #'  
 #' @param obj the output of a \code{qgam()} call. 
 #' @param nbin number of bins used in the internal call to \code{cqcheck()}. 
 #' @param lev the significance levels used by \code{cqcheck()}, which determines the width of the confidence 
 #'            intervals.
 #' @param ... extra arguments to be passed to \code{plot()}
-#' @return Simply produces a plot and prints out some diagnostics.
-#' @details This function provides a single plot, which shows how the number of responses falling below the fitted
+#' @return Simply produces some plots and prints out some diagnostics.
+#' @details This function provides two plots. The first shows how the number of responses falling below the fitted
 #'          quantile (y-axis) changes with the fitted quantile (x-axis). To be clear: if the quantile is fixed to, say, 0.5
-#'          we expect 50\% of the responses to fall below the fit. See \code{?cqcheck()} for details. 
-#'          The function also provide some convergence diagnostics (regarding the optimization), which are the same as in
-#'          \code{mgcv::gam.check}. It reports also the maximum (k') and the selected degrees of freedom of each smooth term.
+#'          we expect 50\% of the responses to fall below the fit. See \code{?cqcheck()} for details. The second plot related
+#'          to \code{|F(hat(mu)) - F(mu0)|}, which is the absolute bias attributable to the fact that qgam is using 
+#'          a smoothed version of the pinball-loss. The absolute bias is evaluated at each observation, and an histogram
+#'          is produced. See Fasiolo et al. (2017) for details. The function also prints out the average absolute bias,
+#'          and the proportion of observations lying below the regression line. It also provides some convergence 
+#'          diagnostics (regarding the optimization), which are the same as in \code{mgcv::gam.check}. 
+#'          It reports also the maximum (k') and the selected degrees of freedom of each smooth term.
 #' @author Matteo Fasiolo <matteo.fasiolo@@gmail.com>, Simon N. Wood. 
+#' @references Fasiolo, M., Goude, Y., Nedellec, R. and Wood, S. N. (2017). Fast calibrated additive quantile regression. 
+#'             Available at \url{https://arxiv.org/abs/1707.03307}.
 #' @examples
 #' library(qgam)
 #' set.seed(0)
@@ -35,6 +41,24 @@ check.qgam <- function(obj,
   cqcheck(obj = obj, v = as.matrix(obj$fitted.values)[ , 1], nbin = nbin, xlab = "Fitted values", ...)
   par(svpar) 
   
+  ## Checking bias induced by having smoothed the loss
+  # Here we are estimating E( Phi(y, mu, lam*sig) - I(y > mu) | x ) using a Gaussian GAM
+  lam <- obj$family$getLam()
+  sig <- exp( obj$family$getTheta() )
+  
+  dat <- obj$model
+  form <- if( is.list(obj$formula) ) { obj$formula[[1]] } else { obj$formula }
+  res <- dat[[ form[[2]] ]] - as.matrix(fit$fitted.values)[ , 1]
+  form[[2]] <- as.symbol( "bias" )
+  
+  dat$bias <- plogis(res, 0, sig*lam) - as.numeric(res > 0)
+  
+  fitBias <- gam(form, data = dat) 
+  hist(fitBias$fitted.values, xlab = expression(F(hat(mu)) - F(mu[0])), main = "Bias due to smoothed loss")
+  
+  cat("Theor. proportion of neg. resid.:", obj$family$getQu(), "  Actual proportion:", mean(res<0))
+  cat("\nMean absolute bias |F(mu) - F(mu0)| =", mean(abs(fitBias$fitted.values)))
+
   ## now summarize convergence information
   cat("\nMethod:",obj$method,"  Optimizer:",obj$optimizer)
   if (!is.null(obj$outer.info)) { ## summarize convergence information
